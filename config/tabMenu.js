78d2b5a602bfc8b5d4ce67a2b051f077ff63987e
datanh14 <37982692+datanh14@users.noreply.github.com>
2020-05-18 20:44:27 +0700

    hello
    day len k loi anb anc :(

--- config/tabMenu.js
+++ config/tabMenu.js
@@ -2,31 +2,33 @@ import * as React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import dessertMenu from '../screens/menuscreens/dessertmenu';
+import mainCourseMenu from '../screens/menuscreens/maincoursemenu';
+import drinkMenu from '../screens/menuscreens/drinkmenu';
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+// function SettingsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
 
 const Tab = createMaterialTopTabNavigator();
 
 export default () => {
   return (
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Home2" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="MainCourse" component={mainCourseMenu} />
+        <Tab.Screen name="Dessert" component={dessertMenu} />
+        <Tab.Screen name="Drink" component={drinkMenu} />
       </Tab.Navigator>
   );
 }
