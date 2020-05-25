949dacb7a825b8bc1e66d163dfdcb639a0f1f969
datanh14 <37982692+datanh14@users.noreply.github.com>
2020-05-25 22:01:19 +0700

    hello
    hello

---
+++ data/menu.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      id: "1",
+      Name: "Chicken",
+      Price: "5",
+      Species: "MainCourse",
+    },
+    {
+        id: "2",
+        Name: "Sandwich",
+        Price: "5",
+        Species: "Dessert",
+      },
+      {
+        id: "3",
+        Name: "Orange",
+        Price: "5",
+        Species: "Drink",
+      },
+    {
+        id: "4",
+        Name: "Fist",
+        Price: "5",
+        Species: "MainCourse",
+    },
+  ];
