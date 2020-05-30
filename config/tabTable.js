3d5a34e8143a95ebc93723e6fb10dae5191d7c10
Fang <38451554+phongcoiydbg99@users.noreply.github.com>
2020-05-30 18:17:17 +0700

    up

--- config/tabTable.js
+++ config/tabTable.js
@@ -97,6 +97,7 @@ export default () => {
               </Text>
             </View>
           ),
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
         }}
       />
     </Tab.Navigator>
