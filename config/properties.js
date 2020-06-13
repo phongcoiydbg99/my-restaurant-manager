d8992e84288c9935df827e51245998142d0a6741
phongcoiydbg99 <phongcoiydbg99@gmail.com>
2020-06-13 13:52:15 +0700

    upload image sever
    Ae cài
    expo install expo-image-picker expo-permissions
    npm install express body-parser
    
    chạy server image mở cmd ở project chạy
    node server
    thay đổi server_image_id ở properties giữ port 3000

--- config/properties.js
+++ config/properties.js
@@ -1,2 +1,3 @@
-export const SERVER_ID = "http://192.168.43.25:8080/";
+export const SERVER_ID = "http://192.168.0.101:8080/";
+export const SERVER_IMAGE_ID = "http://192.168.0.101:3000/";
 
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const fs = require("fs");
+app.listen(3000, () => console.log("Server start"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "15MB" }));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.post("/", (req, res) => {
+  fs.writeFile("./public/"+`${req.body.name}`+".png", req.body.imgsource, "base64", (err) => {
+    if (err) throw err;
+  });
+  res.status(200);
+  console.log("Post complete");
+});
